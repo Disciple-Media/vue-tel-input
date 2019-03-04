97dbc5f4c15823549c6916d3216db327f18e4301
Hubert Dąbrowski <h@dabrow.org>
2019-03-04 11:51:18 +0100

    Add ES6 entry point

---
+++ src/vue-tel-input.es6.js
@@ -0,0 +1,9 @@
+import VueTelInput from "./vue-tel-input.vue";
+
+export default {
+  VueTelInput,
+  install: (Vue) => {
+    Vue.component(VueTelInput.name, VueTelInput);
+    return Vue;
+  },
+};
